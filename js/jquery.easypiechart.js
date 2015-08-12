1854711963fad6083466b54316c5d05c8cd594a2
Luda <ldcg1@hotmail.com>
2015-08-11 23:30:01 -0430

    Logos classes in portfolio added, banner at 100vh, navigation bar updated

--- js/jquery.easypiechart.js
+++ js/jquery.easypiechart.js
@@ -199,7 +199,7 @@ var CanvasRenderer = function(el, options) {
 
 var EasyPieChart = function(el, opts) {
 	var defaultOptions = {
-		barColor: '#ff675f',
+		barColor: '#91CC96',
 		trackColor: '#e1e1e3',
 		scaleColor: '#e1e1e3',
 		scaleLength: 0,
